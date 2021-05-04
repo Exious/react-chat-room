711781425d22e6083c64c905017fd3a072962345
ShiningLight <aizy.fpg@gmail.com>
2021-05-04 20:17:17 +0300

    feat: router logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,37 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { v4 as uuidV4 } from "uuid";
+
 import "./styles/App.css";
 
-import React from "react";
+import Login from "./components/Login";
+import Room from "./components/Room";
 
 function App() {
   const [isLogged, setIsLogged] = React.useState(false);
   const [roomId, setRoomId] = React.useState(null);
 
-  return <div>App</div>;
+  return (
+    <Router>
+      <Route path="/">
+        {isLogged ? (
+          roomId ? (
+            <Redirect to={`/room/${roomId}`} />
+          ) : (
+            <Redirect to={`/room/${uuidV4()}`} />
+          )
+        ) : (
+          <Redirect to="/login" />
+        )}
+      </Route>
+      <Route path="/room/:id" exact>
+        <Room setRoomId={setRoomId} />
+      </Route>
+      <Route path="/login">
+        <Login setIsLogged={setIsLogged} />
+      </Route>
+    </Router>
+  );
 }
 
 export default App;

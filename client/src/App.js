018e6afa8fe1628b58df65ff81aa6ee87e42ee03
ShiningLight <aizy.fpg@gmail.com>
2021-05-04 20:15:07 +0300

    feat: init App comp

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./styles/App.css";
+
+import React from "react";
+
+function App() {
+  const [isLogged, setIsLogged] = React.useState(false);
+  const [roomId, setRoomId] = React.useState(null);
+
+  return <div>App</div>;
+}
+
+export default App;

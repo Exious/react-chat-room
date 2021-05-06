d057747bd2194b3880b91b9aed5a0da58581924b
ShiningLight <aizy.fpg@gmail.com>
2021-05-06 15:33:30 +0300

    feat: server logic impl

--- server/server.js
+++ server/server.js
@@ -43,19 +43,27 @@ function onConnection(socket) {
         new Map([
           ["users", new Map()],
           ["messages", []],
+          ["stream", null],
         ])
       );
     }
-    roomsList.rooms.get(id).get("users").set(user.id, user.name);
+
+    const userObj = {
+      name: user.name,
+      connectedAt: Date.now()
+    }
+    roomsList.rooms.get(id).get("users").set(user.id, Object.values(userObj));
     socket.join(id);
   });
 
   socket.on("ROOM:USER-JOINED", (id) => {
     user.currentlyChat = id;
+
     const obj = {
       userName: user.name,
       roomId: user.currentlyChat,
     };
+
     const message = {
       userName: user.name,
       userId: user.id,
@@ -63,17 +71,25 @@ function onConnection(socket) {
       text: `${user.name} joined`,
       timestamp: Date.now(),
     };
+
     roomsList.rooms.get(user.currentlyChat).get("messages").push(message);
+
     socket.emit("ROOM:USER-DATA", obj);
+
     socket.broadcast
       .to(user.currentlyChat)
       .emit("ROOM:RECIEVE-MESSAGE", message);
+
+    const streamObj = roomsList.rooms.get(user.currentlyChat).get("stream");
+
+    socket.emit("ROOM:STREAMING-CHECK", streamObj);
   });
 
   socket.on("ROOM:FIND-USERS", () => {
     const users = [
       ...roomsList.rooms.get(user.currentlyChat).get("users").values(),
     ];
+
     io.to(user.currentlyChat).emit("ROOM:GET-USERS", users);
   });
 
@@ -87,6 +103,7 @@ function onConnection(socket) {
         text,
         timestamp,
       };
+
       roomsList.rooms
         .get(roomId)
         .get("messages")
@@ -94,7 +111,9 @@ function onConnection(socket) {
           ...obj,
           affiliation: "others",
         });
+
       socket.emit("ROOM:RECIEVE-MESSAGE", { ...obj, affiliation: "my" });
+
       socket.broadcast.to(user.currentlyChat).emit("ROOM:RECIEVE-MESSAGE", {
         ...obj,
         affiliation: "others",
@@ -107,6 +126,36 @@ function onConnection(socket) {
       "ROOM:RECIEVE-MESSAGE-HISTORY",
       roomsList.rooms.get(roomId).get("messages")
     );
+  });
+
+  socket.on("ROOM:STREAMING-START", (stream) => {
+    roomsList.rooms
+      .get(user.currentlyChat)
+      .set("stream", { ...stream, userId: user.currentlyChat });
+
+    const streamObj = roomsList.rooms.get(user.currentlyChat).get("stream");
+
+    socket.broadcast.emit("ROOM:STREAMING-CHECK", streamObj);
+  });
+
+  socket.on("ROOM:STREAM-WANT-TO-WATCH", () => {
+    const streamObj = roomsList.rooms.get(user.currentlyChat).get("stream");
+
+    socket.to(streamObj.userId).emit("ROOM:STREAM-CALL", user.id);
+  });
+
+  socket.on("ROOM:ICE-CANDIDATE", (incoming) => {
+    socket
+      .to(incoming.target)
+      .emit("ROOM:ICE-CANDIDATE-RECIEVED", incoming.candidate);
+  });
+
+  socket.on("ROOM:OFFER", (payload) => {
+    socket.to(payload.target).emit("ROOM:OFFER-RECIEVED", payload);
+  });
+
+  socket.on("ROOM:ANSWER", (payload) => {
+    socket.to(payload.target).emit("ROOM:ANSWER-RECIEVED", payload);
   });
 
   socket.on("disconnect", () => {
